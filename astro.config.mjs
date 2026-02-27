22885f7e631f458e2a5e675d1c7d50061086da17
Nogyboy <guamanc9@gmail.com>
2026-02-26 21:53:52 -0500

    chore: format code and add biome configuration for linting and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
+
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-      site: import.meta.env.DEV ? "http://localhost:4321" : "https://condorexpedition.com/",
-    integrations: [],
-    vite: {
-        plugins: [tailwindcss()],
-    },
+	site: import.meta.env.DEV
+		? "http://localhost:4321"
+		: "https://condorexpedition.com/",
+	integrations: [],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
