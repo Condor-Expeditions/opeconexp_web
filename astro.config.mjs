f17b6158b6d9b706a12d7e0b91db0b6a66c095b8
Nogyboy <guamanc9@gmail.com>
2026-09-22 19:51:51 -0500

    feat: add header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,22 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	site: import.meta.env.DEV
-		? "http://localhost:4321"
-		: "https://condorexpedition.com/",
-	integrations: [],
-	i18n: {
-		defaultLocale: "es",
-		locales: ["es", "en"],
-		routing: {
-			prefixDefaultLocale: false,
-		},
-	},
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: import.meta.env.DEV
+        ? "http://localhost:4321"
+        : "https://condorexpedition.com/",
+    integrations: [icon()],
+    i18n: {
+        defaultLocale: "es",
+        locales: ["es", "en"],
+        routing: {
+            prefixDefaultLocale: false,
+        },
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
